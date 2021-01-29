ff96d8821c6c1fbbc8089353bb0267fa088a83e5
Vinicius Carvalho <vinicius91carvalho@gmail.com>
2021-01-29 12:25:05 -0300

    first class: structuring the zoom project

---
+++ public/pages/room/src/view.js
@@ -0,0 +1,44 @@
+class View {
+    constructor() {
+
+    }
+
+    renderVideo({ userId, stream = null, url = null, isCurrentId = false }) {
+        const video = this.createVideoElement({ srcURL: url, srcStream: stream })
+        this.appendToHTMLTree(userId, video, isCurrentId)
+    }
+
+    createVideoElement({ muted = true, srcURL, srcStream }) {
+        const video = document.createElement('video')
+        video.muted = muted
+        video.src = srcURL
+        video.srcObject = srcStream
+
+        if (srcURL) {
+            video.controls = true
+            video.loop = true
+            Util.sleep(200).then(_ => video.play())
+        }
+
+        if (srcStream) {
+            video.addEventListener("loadedmetadata", _ => video.play())
+        }
+
+        return video
+    }
+
+    appendToHTMLTree(userId, video, isCurrentId) {
+        const div = document.createElement('div')
+        div.id = userId
+        div.classList.add('wrapper')
+        div.append(video)
+
+        const div2 = document.createElement('div')
+        div2.innerText = isCurrentId ? '' : userId
+        div.append(div2)
+
+        const videoGrid = document.getElementById('video-grid')
+        videoGrid.append(div)
+
+    }
+}
